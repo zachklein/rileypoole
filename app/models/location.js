9d002fc32d190670256cebf7c1e60598a35922d8
Dominic Damian <dominic@dominicdamian.com>
2015-02-11 06:48:02 -0800

    README and cleanup

--- app/models/location.js
+++ app/models/location.js
@@ -14,6 +14,3 @@ var LocationSchema   = new Schema({
 });
 
 module.exports = mongoose.model('Location', LocationSchema, 'locations');
-
-
-// {"reference_number":"01001296","resource_name":"Mount Sinai School","address":"1820 Cty. Rd. 57","state":"ALABAMA","county":"Autauga","city":"Prattville","primary_cert":"LI","primary_certdate":20011129,"multiple_property_name":"The Rosenwald School Building Fund and Associated Buildings MPS"},
